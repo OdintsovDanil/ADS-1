e7a4c93db6b48a0e3fed3a98a6ce3605948d651b
OdintsovDanil <99181188+OdintsovDanil@users.noreply.github.com>
2022-02-26 18:58:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,52 +4,17 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value == 2) {
-        return true;
-    }
-    if (value == 0 || value == 1 || value % 2 == 0) {
-        return false;
-    }
-    for (uint64_t i = 3; i <= sqrt(value); i += 2) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    return true;
+// вставьте код функции
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 1) {
-        return 2;
-    }
-    uint64_t check = 1;
-    for (uint64_t i = 3;; i += 2) {
-        if (checkPrime(i)) {
-            ++check;
-        }
-        if (check == n) {
-            return i;
-        }
-    }
+// вставьте код функции
 }
 
 uint64_t nextPrime(uint64_t value) {
-    for (++value;; value++) {
-        if (checkPrime(value)) {
-            return value;
-        }
-    }
+// вставьте код функции
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t summ = 2;
-    if (hbound == 2) {
-        return summ;
-    }
-    for (uint64_t i = 3; i < hbound; i++) {
-        if (checkPrime(i)) {
-            summ += i;
-        }
-    }
-    return summ;
+// вставьте код функции
 }
